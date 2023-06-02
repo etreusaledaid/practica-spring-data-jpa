b5debc7dbafa6dbf9ed95377ae17f1aa63621c41
etreusaledaid <etreusaledaid@gmail.com>
2023-06-02 16:40:29 -0600

    Subiendo archivos

---
+++ java.pizzeria/src/test/java/com/ejemplo/java/pizzeria/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.java.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
